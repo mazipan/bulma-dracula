6ed072009af9742d8d9b8d576223a8229e10c8e6
Irfan Maulana <maulana.irfan@tokopedia.com>
2020-09-24 14:52:09 +0700

    feat: expose vars

--- README.md
+++ README.md
@@ -25,6 +25,7 @@ $ npm i bulma-dracula
 Import in your `.scss` files:
 
 ```scss
+// this is already bundled with bulma.css
 @import "~bulma-dracula/src/bulma-dracula.scss";
 ```
 
@@ -34,24 +35,45 @@ CDN on `unpkg`:
 
 Change `1.0.0` with latest version or you can just hit [https://unpkg.com/bulma-dracula](https://unpkg.com/bulma-dracula)
 
+## Override Variables
+
+Bulma Dracula already bundled with Bulma CSS, that's why it can not be overriden.
+You can override with only use our SCSS variable definitions.
+
+- Install Bulma and Bulma Dracula seperately:
+
+```bash
+$ yarn add bulma bulma-dracula
+# OR
+$ npm i bulma bulma-dracula
+```
+
+- Use Bulma Dracula variables:
+
+```scss
+@import "~bulma-dracula/src/bulma-dracula-vars.scss";
+// You can override the default Bulma Dracula variables here
+@import "~bulma/bulma";
+```
+
 ## Dracula Color Palette
 
 This repo will override these variables https://bulma.io/documentation/customize/variables/ with below palletes:
 
-Palette      | Hex       | RGB           | HSL             | 
----          | ---       | ---           | ---             | 
-Background   | `#282a36` | `40 42 54`    | `231° 15% 18%`  | 
-Current Line | `#44475a` | `68 71 90`    | `232° 14% 31%`  | 
-Selection    | `#44475a` | `68 71 90`    | `232° 14% 31%`  | 
-Foreground   | `#f8f8f2` | `248 248 242` | `60° 30% 96%`   | 
-Comment      | `#6272a4` | `98 114 164`  | `225° 27% 51%`  | 
-Cyan         | `#8be9fd` | `139 233 253` | `191° 97% 77%`  | 
-Green        | `#50fa7b` | `80 250 123`  | `135° 94% 65%`  | 
-Orange       | `#ffb86c` | `255 184 108` | `31° 100% 71%`  | 
-Pink         | `#ff79c6` | `255 121 198` | `326° 100% 74%` | 
-Purple       | `#bd93f9` | `189 147 249` | `265° 89% 78%`  | 
-Red          | `#ff5555` | `255 85 85`   | `0° 100% 67%`   | 
-Yellow       | `#f1fa8c` | `241 250 140` | `65° 92% 76%`   | 
+Palette      | Hex       | RGB           | HSL             |
+---          | ---       | ---           | ---             |
+Background   | `#282a36` | `40 42 54`    | `231° 15% 18%`  |
+Current Line | `#44475a` | `68 71 90`    | `232° 14% 31%`  |
+Selection    | `#44475a` | `68 71 90`    | `232° 14% 31%`  |
+Foreground   | `#f8f8f2` | `248 248 242` | `60° 30% 96%`   |
+Comment      | `#6272a4` | `98 114 164`  | `225° 27% 51%`  |
+Cyan         | `#8be9fd` | `139 233 253` | `191° 97% 77%`  |
+Green        | `#50fa7b` | `80 250 123`  | `135° 94% 65%`  |
+Orange       | `#ffb86c` | `255 184 108` | `31° 100% 71%`  |
+Pink         | `#ff79c6` | `255 121 198` | `326° 100% 74%` |
+Purple       | `#bd93f9` | `189 147 249` | `265° 89% 78%`  |
+Red          | `#ff5555` | `255 85 85`   | `0° 100% 67%`   |
+Yellow       | `#f1fa8c` | `241 250 140` | `65° 92% 76%`   |
 
 
 ## Support me
--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
-var path = require('path')
-var webpack = require('webpack')
-const ExtractTextPlugin = require('extract-text-webpack-plugin')
-const HtmlWebpackPlugin = require('html-webpack-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
+var path = require('path');
+var webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 
 const publicPath = 'bulma-dracula';
 
@@ -16,45 +16,67 @@ const extractHTML = new HtmlWebpackPlugin({
     html5: true,
     minifyCSS: true,
     removeComments: true,
-    removeEmptyAttributes: true
+    removeEmptyAttributes: true,
   },
-  environment: process.env.NODE_ENV
-})
+  environment: process.env.NODE_ENV,
+});
 
 module.exports = {
   entry: {
-    app: './src/index.js'
+    app: './src/index.js',
   },
   output: {
     path: path.resolve(__dirname, 'dist'),
     publicPath: `/${publicPath}`,
-    filename: '[name].[hash].js'
+    filename: '[name].[hash].js',
   },
   mode: process.env.NODE_ENV,
   resolve: {
     extensions: ['.js', '.vue'],
     alias: {
-      'src': path.resolve(__dirname, 'dist')
-    }
+      src: path.resolve(__dirname, 'dist'),
+    },
   },
   module: {
     rules: [
-    {
-      test: /\.scss$/,
-      use: ExtractTextPlugin.extract({
-        fallback: 'style-loader',
-        use: ['css-loader', 'sass-loader']
-      })
-    },
-    {
-      test: /\.js$/,
-      exclude: path.resolve(__dirname, 'node_modules'),
-      loader: 'babel-loader',
-      options: {
-        presets: ['@babel/preset-env']
-      }
-    }
-    ]
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: process.env.NODE_ENV === 'development',
+            },
+          },
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              // only enable hot in development
+              hmr: process.env.NODE_ENV === 'development',
+              // if hmr does not work, this is a forceful method.
+              reloadAll: true,
+            },
+          },
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.js$/,
+        exclude: path.resolve(__dirname, 'node_modules'),
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+        },
+      },
+    ],
   },
   devServer: {
     historyApiFallback: true,
@@ -62,25 +84,30 @@ module.exports = {
     openPage: `${publicPath}/`,
   },
   performance: {
-    hints: false
+    hints: false,
   },
   plugins: [
-    new ExtractTextPlugin('bulma-dracula.css'),
-    extractHTML,
-    new CopyPlugin([
-      { from: 'static', to: '' },
-    ]),
-  ]
-}
+    new MiniCssExtractPlugin({
+      filename: 'bulma-dracula.css',
+      ignoreOrder: false,
+    }),
+		extractHTML,
+		new CopyPlugin({
+      patterns: [
+        { from: 'static', to: '' },
+      ],
+    }),
+  ],
+};
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.devtool = '#source-map'
+  module.exports.devtool = '#source-map';
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: '"production"'
-      }
-    })
-  ])
+        NODE_ENV: '"production"',
+      },
+    }),
+  ]);
 }
